cacb35aa8c60b270db8a0639402577e8aafcdee7
akshata2005 <akshatasawant1805@gmail.com>
2022-01-26 19:16:58 +0530

    [akshata] program of FibinacciSeries

---
+++ LogicalProgramms/LogicalProgramms/FibonacciSeries.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public class FibonacciSeries
+    {
+        public static void FindFibonacciSeries()
+        {
+            /*..................................................
+             * initialize three variables n1,n2,n3 and assign n1=0 and n2=1 as Fibonacci series require two values
+             * .....................................................................*/
+            int n1 = 0, n2 = 1, n3;
+            Console.WriteLine("Enter a number to find Fibonacci Series :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < num; i++)
+            {
+                /*..........................................................
+                 * Store the sum of n1 & n2 in n3 and print n3
+                 * repeat the below process until i reaches n
+                 * .................................................*/
+                n3 = n1 + n2;
+                Console.Write(n3 + ",");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
---
+++ LogicalProgramms/LogicalProgramms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems ..........");
+            /*...........................................................
+             * choose an option to perform particular operation
+             * .................................................*/
+            Console.WriteLine("Enter Your Choice :");
+            int options = Convert.ToInt32(Console.ReadLine());
+            /*..............................................................
+             * Choose options using switch case
+             * ................................................*/
+            switch (options)
+            {
+                 case 1:
+                     Console.WriteLine("Fibonacci Series ......");
+                     FibonacciSeries.FindFibonacciSeries();
+                     break;
+                default:
+                    break;
+            }
+        }
+    }
+}
